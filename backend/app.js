3b85c6d9e1fe1807102ac8b565ee95ffbcb53b6e
Aksenov-m <aksenov.m87@yandex.ru>
2022-05-16 22:17:57 +0300

    refactor: Устраненить замечания_03_15

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const { celebrate, errors, Joi } = require('celebrate');
 const userRouter = require('./routes/users'); // импортируем роутер user
 const cardRouter = require('./routes/cards'); // импортируем роутер Card
@@ -10,7 +10,7 @@ const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-err');
 const regex = require('./utils/utils');
-// const errorHandling = require('./middlewares/errorHandling');
+const errorHandling = require('./middlewares/errorHandling');
 
 const {
   login,
@@ -58,8 +58,8 @@ app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(errors()); // обработчик ошибок celebrate
 
-// app.use(errorHandling);
-app.use(helmet());
+app.use(errorHandling);
+// app.use(helmet());
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
